884b6069de8fafafa03234b1a1212a707e2b9eb3
barilo <e.barilo@isida.by>
2023-10-20 16:27:07 +0300

    bulk discounts load

--- src/main/java/tgb/btc/library/util/BulkDiscountUtil.java
+++ src/main/java/tgb/btc/library/util/BulkDiscountUtil.java
@@ -1,12 +1,17 @@
 package tgb.btc.library.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import tgb.btc.library.constants.enums.bot.DealType;
 import tgb.btc.library.constants.enums.bot.FiatCurrency;
+import tgb.btc.library.constants.enums.properties.CommonProperties;
+import tgb.btc.library.exception.PropertyValueNotFoundException;
 import tgb.btc.library.vo.BulkDiscount;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,4 +34,35 @@ public final class BulkDiscountUtil {
         return BigDecimal.ZERO;
     }
 
+    static {
+        for (String key : CommonProperties.BULK_DISCOUNT.getKeys()) {
+            int sum;
+            if (StringUtils.isBlank(key)) {
+                throw new PropertyValueNotFoundException("Не указано название для одного из ключей" + key + ".");
+            }
+            try {
+                sum = Integer.parseInt(key.split("\\.")[2]);
+            } catch (NumberFormatException e) {
+                throw new PropertyValueNotFoundException("Не корректное название для ключа " + key + ".");
+            }
+            String value =  CommonProperties.BULK_DISCOUNT.getString(key);
+            if (StringUtils.isBlank(value)) {
+                throw new PropertyValueNotFoundException("Не указано значение для ключа " + key + ".");
+            }
+            double percent;
+            try {
+                percent = Double.parseDouble(value);
+            } catch (NumberFormatException e) {
+                throw new PropertyValueNotFoundException("Не корректное значение для ключа " + key + ".");
+            }
+            BULK_DISCOUNTS.add(BulkDiscount.builder()
+                    .percent(percent)
+                    .sum(sum)
+                    .fiatCurrency(FiatCurrency.getByCode(key.split("\\.")[0]))
+                    .dealType(DealType.findByKey((key.split("\\.")[1])))
+                    .build());
+        }
+        BULK_DISCOUNTS.sort(Comparator.comparingInt(BulkDiscount::getSum));
+        Collections.reverse(BULK_DISCOUNTS);
+    }
 }
