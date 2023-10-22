2a525723535cdb2bb3e0323a3b8adc46ced1f0fd
barilo <barilo>
2023-10-22 21:37:49 +0300

    find all method

--- src/main/java/tgb/btc/library/repository/bot/paging/PagingDealRepository.java
+++ src/main/java/tgb/btc/library/repository/bot/paging/PagingDealRepository.java
@@ -1,7 +1,6 @@
 package tgb.btc.library.repository.bot.paging;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +11,6 @@ import tgb.btc.library.constants.enums.bot.DealStatus;
 @Transactional
 public interface PagingDealRepository extends PagingAndSortingRepository<Deal, Long> {
 
-    void findAllByDealStatusNot(DealStatus dealStatus, Pageable pageable, Sort sort);
+    void findAllByDealStatusNot(DealStatus dealStatus, Pageable pageable);
 
 }
