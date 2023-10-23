9af9f21c478ef14546981126a21de5b6710e7237
barilo <e.barilo@isida.by>
2023-10-23 13:56:44 +0300

    deal status colors

--- src/main/java/tgb/btc/library/constants/enums/bot/DealStatus.java
+++ src/main/java/tgb/btc/library/constants/enums/bot/DealStatus.java
@@ -7,21 +7,28 @@ import tgb.btc.library.util.web.JacksonUtil;
 import java.util.function.Function;
 
 public enum DealStatus implements ObjectNodeConvertable<DealStatus> {
-    NEW("Новая"),
-    PAID("Оплачено"),
-    AWAITING_VERIFICATION("Ожидает верификацию"),
-    VERIFICATION_REJECTED("Верификация отклонена"),
-    VERIFICATION_RECEIVED("Верификация получена"),
-    CONFIRMED("Подтверждена");
+    NEW("Новая", "blackColor"),
+    PAID("Оплачено", "mediumSeaGreenColor"),
+    AWAITING_VERIFICATION("Ожидает верификацию", "orangeColor"),
+    VERIFICATION_REJECTED("Верификация отклонена", "redColor"),
+    VERIFICATION_RECEIVED("Верификация получена", "violetColor"),
+    CONFIRMED("Подтверждена", "lightGrey");
 
     private final String displayName;
 
-    DealStatus(String displayName) {
+    private final String color;
+
+    DealStatus(String displayName, String color) {
         this.displayName = displayName;
+        this.color = color;
     }
 
     public String getDisplayName() {
         return displayName;
+    }
+
+    public String getColor() {
+        return color;
     }
 
     @Override
