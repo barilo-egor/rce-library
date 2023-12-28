9e24e01c71ec8a0a399117ea33a91ad45e004423
barilo <e.barilo@isida.by>
2023-12-28 11:52:01 +0300

    new constructor

--- src/main/java/tgb/btc/library/exception/BaseException.java
+++ src/main/java/tgb/btc/library/exception/BaseException.java
@@ -7,4 +7,9 @@ public class BaseException extends RuntimeException{
     public BaseException(String message) {
         super(message);
     }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
