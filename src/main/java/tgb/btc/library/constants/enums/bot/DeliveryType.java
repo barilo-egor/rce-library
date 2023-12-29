a72ccbef5d86e0a8a511ce60f56385bfceb8d4a7
barilo <e.barilo@isida.by>
2023-12-29 14:09:06 +0300

    delivery type string field

--- src/main/java/tgb/btc/library/constants/enums/bot/DeliveryType.java
+++ src/main/java/tgb/btc/library/constants/enums/bot/DeliveryType.java
@@ -1,6 +1,16 @@
 package tgb.btc.library.constants.enums.bot;
 
 public enum DeliveryType {
-    VIP,
-    STANDARD;
+    VIP("Вип"),
+    STANDARD("Обычный");
+
+    final String displayName;
+
+    DeliveryType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
