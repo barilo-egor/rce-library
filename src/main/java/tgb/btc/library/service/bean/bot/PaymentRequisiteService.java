a822e11d375c660429b91a180f03767d1af19569
barilo <e.barilo@isida.by>
2024-01-08 15:29:10 +0300

    fix index order

--- src/main/java/tgb/btc/library/service/bean/bot/PaymentRequisiteService.java
+++ src/main/java/tgb/btc/library/service/bean/bot/PaymentRequisiteService.java
@@ -46,7 +46,8 @@ public class PaymentRequisiteService {
                 PAYMENT_REQUISITE_ORDER.put(paymentTypePid, order);
             } else {
                 Integer paymentTypeRequisitesSize = paymentRequisiteRepository.countByPaymentTypePid(paymentTypePid);
-                if (Objects.isNull(paymentTypeRequisitesSize) || paymentTypeRequisitesSize.equals(order + 1)) PAYMENT_REQUISITE_ORDER.put(paymentTypePid, 1);
+                if (Objects.isNull(paymentTypeRequisitesSize) || paymentTypeRequisitesSize.equals(order + 1))
+                    PAYMENT_REQUISITE_ORDER.put(paymentTypePid, 0);
                 else PAYMENT_REQUISITE_ORDER.put(paymentTypePid, order + 1);
             }
         }
