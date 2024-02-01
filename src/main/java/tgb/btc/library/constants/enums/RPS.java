d4be37660613adb66abea71b9be2faad9c39bd0f
hodikov <khodikove18@mail.ru>
2024-02-01 09:05:47 +0300

    123

---
+++ src/main/java/tgb/btc/library/constants/enums/RPS.java
@@ -0,0 +1,17 @@
+package tgb.btc.library.constants.enums;
+
+import tgb.btc.library.constants.enums.properties.PropertiesPath;
+import tgb.btc.library.interfaces.Module;
+
+public enum RPS implements Module {
+
+    NONE,
+    STANDARD,
+    STANDARD_ADMIN;
+
+    @Override
+    public boolean isCurrent() {
+        return this.equals(valueOf(PropertiesPath.GAMES_PROPERTIES.getString("rock.paper.scissors", "NONE")));
+    }
+
+}
