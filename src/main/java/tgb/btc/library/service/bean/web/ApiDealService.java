d923e954eee2f4178a9a10290f8fab9c0bf8a187
grewg <grew.gh@mail.ru>
2024-02-24 22:51:24 +0300

    RCE-104 Добавить Api-сделки в отчет

--- src/main/java/tgb/btc/library/service/bean/web/ApiDealService.java
+++ src/main/java/tgb/btc/library/service/bean/web/ApiDealService.java
@@ -2,7 +2,13 @@ package tgb.btc.library.service.bean.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tgb.btc.library.bean.web.api.ApiDeal;
+import tgb.btc.library.constants.enums.web.ApiDealStatus;
 import tgb.btc.library.repository.web.ApiDealRepository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @Service
 public class ApiDealService {
@@ -13,4 +19,23 @@ public class ApiDealService {
     public void setApiDealRepository(ApiDealRepository apiDealRepository) {
         this.apiDealRepository = apiDealRepository;
     }
+
+    public List<ApiDeal> getAcceptedByDate(LocalDateTime dateTime) {
+        LocalDateTime startDay = dateTime.truncatedTo(ChronoUnit.DAYS);
+        LocalDateTime endDay = dateTime
+                .plus(1, ChronoUnit.DAYS)
+                .truncatedTo(ChronoUnit.DAYS)
+                .minusNanos(1);
+        return apiDealRepository.getByDateBetween(startDay, endDay, ApiDealStatus.ACCEPTED);
+    }
+
+    public List<ApiDeal> getAcceptedByDateBetween(LocalDateTime start, LocalDateTime end) {
+        LocalDateTime startDay = start.truncatedTo(ChronoUnit.DAYS);
+        LocalDateTime endDay = end
+                .plus(1, ChronoUnit.DAYS)
+                .truncatedTo(ChronoUnit.DAYS)
+                .minusNanos(1);
+        return apiDealRepository.getByDateBetween(startDay, endDay, ApiDealStatus.ACCEPTED);
+    }
+
 }
