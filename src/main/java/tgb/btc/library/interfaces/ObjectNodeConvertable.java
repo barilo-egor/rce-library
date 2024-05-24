509ff1c688d8e03525109fd1501b2e499a631b14
barilo <e.barilo@isida.by>
2024-05-24 12:30:29 +0300

    json convertable

--- src/main/java/tgb/btc/library/interfaces/ObjectNodeConvertable.java
+++ src/main/java/tgb/btc/library/interfaces/ObjectNodeConvertable.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.function.Function;
 
+@FunctionalInterface
 public interface ObjectNodeConvertable<T> {
 
     Function<T, ObjectNode> mapFunction();
