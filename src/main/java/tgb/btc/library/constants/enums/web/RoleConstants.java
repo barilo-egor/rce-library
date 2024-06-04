ab2e39d75a13abfdb82f358443d2a49e28975d03
barilo <e.barilo@isida.by>
2024-06-04 18:18:44 +0300

    getAvailable roles

--- src/main/java/tgb/btc/library/constants/enums/web/RoleConstants.java
+++ src/main/java/tgb/btc/library/constants/enums/web/RoleConstants.java
@@ -1,9 +1,12 @@
 package tgb.btc.library.constants.enums.web;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import tgb.btc.library.exception.BaseException;
 import tgb.btc.library.interfaces.ObjectNodeConvertable;
 import tgb.btc.library.util.web.JacksonUtil;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 
 public enum RoleConstants implements ObjectNodeConvertable<RoleConstants> {
@@ -27,5 +30,15 @@ public enum RoleConstants implements ObjectNodeConvertable<RoleConstants> {
         return roleConstants -> JacksonUtil.getEmpty()
                 .put("name", roleConstants.name())
                 .put("displayName", roleConstants.getDisplayName());
+    }
+
+    public static final List<RoleConstants> ALL = List.of(RoleConstants.values());
+    public static final List<RoleConstants> AVAILABLE_TO_OPERATOR = List.of(ROLE_USER, ROLE_API_CLIENT, ROLE_OPERATOR);
+
+    public static List<RoleConstants> getAvailable(RoleConstants roleConstants) {
+        if (Objects.isNull(roleConstants)) return ALL;
+        if (RoleConstants.ROLE_ADMIN.equals(roleConstants)) return AVAILABLE_TO_OPERATOR;
+        if (RoleConstants.ROLE_OPERATOR.equals(roleConstants)) return AVAILABLE_TO_OPERATOR;
+        throw new BaseException("Доступ к ролям для " + roleConstants.name() + " не настроен.");
     }
 }
