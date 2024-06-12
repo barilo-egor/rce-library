fead1f5bfe7a99e8e834c809ecdeaafb2024907a
barilo <barilo>
2024-06-12 20:46:38 +0300

    DealPaymentRepository

--- src/main/java/tgb/btc/library/repository/web/DealPaymentRepository.java
+++ src/main/java/tgb/btc/library/repository/web/DealPaymentRepository.java
@@ -3,9 +3,10 @@ package tgb.btc.library.repository.web;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tgb.btc.library.bean.bot.DealPayment;
+import tgb.btc.library.repository.BaseRepository;
 
 @Repository
-public interface DealPaymentRepository {
+public interface DealPaymentRepository extends BaseRepository<DealPayment>  {
 
     /**
      * SELECT
