fff3306ea3eaaaf75259c2c795dc50a84a53666f
barilo <e.barilo@isida.by>
2024-06-19 16:24:03 +0300

    rework

--- src/main/java/tgb/btc/library/bean/web/api/ApiCalculation.java
+++ src/main/java/tgb/btc/library/bean/web/api/ApiCalculation.java
@@ -5,9 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import tgb.btc.library.bean.BasePersist;
-import tgb.btc.library.bean.bot.Deal;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 import java.util.Objects;
 
@@ -19,17 +21,11 @@ import java.util.Objects;
 @AllArgsConstructor
 public class ApiCalculation extends BasePersist {
 
-    @OneToOne
-    private Deal dealFrom;
-
-    @OneToOne
-    private Deal dealTo;
+    @OneToMany
+    private List<ApiDeal> deals;
 
     @ManyToOne
     private ApiUser apiUser;
-
-    @OneToMany
-    private List<ApiCalculationData> apiCalculationData;
 
     @Override
     public boolean equals(Object o) {
@@ -40,14 +36,12 @@ public class ApiCalculation extends BasePersist {
         if (!super.equals(o))
             return false;
         ApiCalculation that = (ApiCalculation) o;
-        return Objects.equals(dealFrom, that.dealFrom) && Objects.equals(dealTo, that.dealTo)
-                && Objects.equals(apiUser, that.apiUser) && Objects.equals(apiCalculationData,
-                that.apiCalculationData);
+        return Objects.equals(deals, that.deals) && Objects.equals(apiUser, that.apiUser);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), dealFrom, dealTo, apiUser, apiCalculationData);
+        return Objects.hash(super.hashCode(), deals, apiUser);
     }
 
 }
---
+++ src/main/java/tgb/btc/library/repository/web/ApiCalculationRepository.java
@@ -0,0 +1,10 @@
+package tgb.btc.library.repository.web;
+
+import org.springframework.stereotype.Repository;
+import tgb.btc.library.bean.web.api.ApiCalculation;
+import tgb.btc.library.repository.BaseRepository;
+
+@Repository
+public interface ApiCalculationRepository extends BaseRepository<ApiCalculation> {
+
+}
