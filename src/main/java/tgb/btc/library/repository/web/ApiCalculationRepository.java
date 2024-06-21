03f50de6bef8109c35720da059a87dde724a48bf
barilo <e.barilo@isida.by>
2024-06-21 11:30:40 +0300

    getByApiUserPid

--- src/main/java/tgb/btc/library/repository/web/ApiCalculationRepository.java
+++ src/main/java/tgb/btc/library/repository/web/ApiCalculationRepository.java
@@ -1,10 +1,15 @@
 package tgb.btc.library.repository.web;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tgb.btc.library.bean.web.api.ApiCalculation;
 import tgb.btc.library.repository.BaseRepository;
 
+import java.util.List;
+
 @Repository
 public interface ApiCalculationRepository extends BaseRepository<ApiCalculation> {
 
+    @Query("from ApiCalculation where apiUser.pid=:pid")
+    List<ApiCalculation> getByApiUserPid(Long pid);
 }
