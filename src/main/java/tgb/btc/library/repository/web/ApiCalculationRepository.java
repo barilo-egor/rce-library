6188e240a529229927d50482e9a38dbeb5937735
barilo <barilo>
2024-06-22 16:56:07 +0300

    ApiCalculationRepository

--- src/main/java/tgb/btc/library/repository/web/ApiCalculationRepository.java
+++ src/main/java/tgb/btc/library/repository/web/ApiCalculationRepository.java
@@ -1,21 +1,20 @@
 package tgb.btc.library.repository.web;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import tgb.btc.library.bean.web.api.ApiCalculation;
 import tgb.btc.library.bean.web.api.ApiUser;
+import tgb.btc.library.repository.BaseRepository;
 
 import java.util.List;
 
 @Repository
-public interface ApiCalculationRepository extends PagingAndSortingRepository<ApiCalculation, Long> {
+public interface ApiCalculationRepository extends BaseRepository<ApiCalculation> {
 
     @Query("from ApiCalculation where apiUser.pid=:pid")
     List<ApiCalculation> getByApiUserPid(Long pid);
 
-    List<ApiCalculation> findAllByApiUser(ApiUser apiUser, Pageable pageable);
+    List<ApiCalculation> findAllByApiUser(ApiUser apiUser);
 
     Long countAllByApiUser(ApiUser apiUser);
 }
