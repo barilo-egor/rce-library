fe275114c063d602bc3c5020fb1137ab6fd7242f
barilo <barilo>
2024-06-26 23:07:38 +0300

    DateDealService

---
+++ src/main/java/tgb/btc/library/service/bean/bot/deal/read/DateDealService.java
@@ -0,0 +1,47 @@
+package tgb.btc.library.service.bean.bot.deal.read;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import tgb.btc.library.bean.bot.Deal;
+import tgb.btc.library.interfaces.service.bot.deal.read.IDateDealService;
+import tgb.btc.library.repository.BaseRepository;
+import tgb.btc.library.repository.bot.deal.read.DateDealRepository;
+import tgb.btc.library.service.bean.BasePersistService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class DateDealService extends BasePersistService<Deal> implements IDateDealService {
+
+    private DateDealRepository dealRepository;
+
+    @Autowired
+    public void setDealRepository(DateDealRepository dealRepository) {
+        this.dealRepository = dealRepository;
+    }
+
+    @Autowired
+    public DateDealService(BaseRepository<Deal> baseRepository) {
+        super(baseRepository);
+    }
+
+    @Override
+    public List<Deal> getByDateBetween(LocalDate startDate, LocalDate endDate) {
+        return dealRepository.getByDateBetween(startDate, endDate);
+    }
+
+    @Override
+    public List<Deal> getByDateTimeBetween(LocalDateTime startDate, LocalDateTime endDate) {
+        return dealRepository.getByDateTimeBetween(startDate, endDate);
+    }
+
+    @Override
+    public List<Deal> getPassedByDate(LocalDate date) {
+        return dealRepository.getPassedByDate(date);
+    }
+
+    @Override
+    public List<Deal> getPassedByDateTimeBetween(LocalDateTime startDate, LocalDateTime endDate) {
+        return dealRepository.getPassedByDateTimeBetween(startDate, endDate);
+    }
+}
