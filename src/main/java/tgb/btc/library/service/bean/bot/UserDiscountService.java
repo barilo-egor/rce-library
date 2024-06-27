499769b5dc824fcb7506df30c385330bdde3ce65
barilo <barilo>
2024-06-27 23:20:39 +0300

    UserDiscountService

--- src/main/java/tgb/btc/library/service/bean/bot/UserDiscountService.java
+++ src/main/java/tgb/btc/library/service/bean/bot/UserDiscountService.java
@@ -3,12 +3,15 @@ package tgb.btc.library.service.bean.bot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tgb.btc.library.bean.bot.UserDiscount;
+import tgb.btc.library.interfaces.service.bot.IUserDiscountService;
 import tgb.btc.library.repository.BaseRepository;
 import tgb.btc.library.repository.bot.UserDiscountRepository;
 import tgb.btc.library.service.bean.BasePersistService;
 
+import java.math.BigDecimal;
+
 @Service
-public class UserDiscountService extends BasePersistService<UserDiscount> {
+public class UserDiscountService extends BasePersistService<UserDiscount> implements IUserDiscountService {
 
     private UserDiscountRepository userDiscountRepository;
 
@@ -22,7 +25,52 @@ public class UserDiscountService extends BasePersistService<UserDiscount> {
         super(baseRepository);
     }
 
+    @Override
+    public Boolean getRankDiscountByUserChatId(Long chatId) {
+        return userDiscountRepository.getRankDiscountByUserChatId(chatId);
+    }
+
+    @Override
+    public BigDecimal getPersonalBuyByChatId(Long chatId) {
+        return userDiscountRepository.getPersonalBuyByChatId(chatId);
+    }
+
+    @Override
+    public BigDecimal getPersonalSellByChatId(Long chatId) {
+        return userDiscountRepository.getPersonalSellByChatId(chatId);
+    }
+
+    @Override
+    public Long countByUser_Pid(Long userPid) {
+        return userDiscountRepository.countByUser_Pid(userPid);
+    }
+
+    @Override
+    public UserDiscount getByUserChatId(Long chatId) {
+        return userDiscountRepository.getByUserChatId(chatId);
+    }
+
     public boolean isExistByUserPid(Long userPid) {
         return userDiscountRepository.countByUser_Pid(userPid) > 0;
+    }
+
+    @Override
+    public void updateIsRankDiscountOnByPid(Boolean isRankDiscountOn, Long pid) {
+        userDiscountRepository.updateIsRankDiscountOnByPid(isRankDiscountOn, pid);
+    }
+
+    @Override
+    public void updatePersonalBuyByUserPid(BigDecimal personalBuy, Long userPid) {
+        userDiscountRepository.updatePersonalBuyByUserPid(personalBuy, userPid);
+    }
+
+    @Override
+    public void updatePersonalSellByUserPid(BigDecimal personalSell, Long userPid) {
+        userDiscountRepository.updatePersonalSellByUserPid(personalSell, userPid);
+    }
+
+    @Override
+    public void deleteByUser_ChatId(Long userChatId) {
+        userDiscountRepository.deleteByUser_ChatId(userChatId);
     }
 }
