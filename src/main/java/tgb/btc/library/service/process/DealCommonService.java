23882403f030ef4b49e1a3585d3aacb79220a1e8
barilo <e.barilo@isida.by>
2024-06-28 15:24:17 +0300

    DealCommonService

--- src/main/java/tgb/btc/library/service/process/DealCommonService.java
+++ src/main/java/tgb/btc/library/service/process/DealCommonService.java
@@ -1,5 +1,6 @@
 package tgb.btc.library.service.process;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tgb.btc.library.interfaces.process.IDealCommonService;
 import tgb.btc.library.interfaces.service.bot.deal.read.IDealCountService;
@@ -8,6 +9,11 @@ import tgb.btc.library.interfaces.service.bot.deal.read.IDealCountService;
 public class DealCommonService implements IDealCommonService {
 
     private IDealCountService dealCountService;
+
+    @Autowired
+    public void setDealCountService(IDealCountService dealCountService) {
+        this.dealCountService = dealCountService;
+    }
 
     @Override
     public boolean isFirstDeal(Long chatId) {
