1a651d185cbc4079c00af3185551dcacba51c7fe
barilo <e.barilo@isida.by>
2024-06-28 14:50:40 +0300

    ReferralUserService fix

--- src/main/java/tgb/btc/library/service/bean/bot/ReferralUserService.java
+++ src/main/java/tgb/btc/library/service/bean/bot/ReferralUserService.java
@@ -3,12 +3,13 @@ package tgb.btc.library.service.bean.bot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tgb.btc.library.bean.bot.ReferralUser;
+import tgb.btc.library.interfaces.service.bot.IReferralUserService;
 import tgb.btc.library.repository.BaseRepository;
 import tgb.btc.library.repository.bot.ReferralUserRepository;
 import tgb.btc.library.service.bean.BasePersistService;
 
 @Service
-public class ReferralUserService extends BasePersistService<ReferralUser> {
+public class ReferralUserService extends BasePersistService<ReferralUser> implements IReferralUserService {
     private final ReferralUserRepository referralUserRepository;
 
     @Autowired
