5b3baf2d4576f9264366e472c9847869b17e69d3
barilo <e.barilo@isida.by>
2024-07-09 18:17:57 +0300

    JsonConvertable

--- src/main/java/tgb/btc/library/bean/bot/GroupChat.java
+++ src/main/java/tgb/btc/library/bean/bot/GroupChat.java
@@ -1,10 +1,13 @@
 package tgb.btc.library.bean.bot;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import tgb.btc.library.bean.BasePersist;
 import tgb.btc.library.constants.enums.MemberStatus;
 import tgb.btc.library.constants.enums.bot.GroupChatType;
+import tgb.btc.library.interfaces.JsonConvertable;
+import tgb.btc.library.util.web.JacksonUtil;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,7 +19,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class GroupChat extends BasePersist {
+public class GroupChat extends BasePersist implements JsonConvertable {
 
     @Getter
     @Setter
@@ -50,4 +53,12 @@ public class GroupChat extends BasePersist {
     @Column(nullable = false)
     @ColumnDefault("true")
     private Boolean isSendMessageEnabled;
+
+    @Override
+    public ObjectNode map() {
+        return JacksonUtil.getEmpty()
+                .put("title", this.getTitle())
+                .put("pid", this.getPid());
+    }
+
 }
