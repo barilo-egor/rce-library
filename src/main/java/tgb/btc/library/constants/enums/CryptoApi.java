0c8e2a39702f7ad67a6cc2662e75aead886baf29
barilo <barilo>
2024-07-15 22:25:56 +0300

    hotfix

--- src/main/java/tgb/btc/library/constants/enums/CryptoApi.java
+++ src/main/java/tgb/btc/library/constants/enums/CryptoApi.java
@@ -2,6 +2,7 @@ package tgb.btc.library.constants.enums;
 
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -17,28 +18,94 @@ import java.util.function.Function;
 @Slf4j
 public enum CryptoApi {
     BTC_USD_BLOCKCHAIN("https://api.blockchain.com/v3/exchange/tickers/BTC-USD",
-            jsonObject -> jsonObject.get("last_trade_price")),
+            jsonObject -> {
+                try {
+                    return jsonObject.get("last_trade_price");
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            }),
     BTC_USD_BINANCE("https://api1.binance.com/api/v3/avgPrice?symbol=BTCUSDT",
-            jsonObject -> jsonObject.get("price")),
+            jsonObject -> {
+                try {
+                    return jsonObject.get("price");
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            }),
     LTC_USD_BINANCE("https://api1.binance.com/api/v3/avgPrice?symbol=LTCUSDT",
-            jsonObject -> jsonObject.get("price")),
+            jsonObject -> {
+                try {
+                    return jsonObject.get("price");
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            }),
     LTC_USD_BLOCKCHAIN("https://api.blockchain.com/v3/exchange/tickers/LTC-USD",
-            jsonObject -> jsonObject.get("last_trade_price")),
+            jsonObject -> {
+                try {
+                    return jsonObject.get("last_trade_price");
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            }),
     XMR_USD_BINANCE("https://api.binance.com/api/v3/ticker/price?symbol=XMRUSDT",
-            jsonObject -> jsonObject.get("price")),
+            jsonObject -> {
+                try {
+                    return jsonObject.get("price");
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            }),
     XMR_USD_KRAKEN("https://api.kraken.com/0/public/Ticker?pair=XMRUSD",
-            jsonObject -> ((JSONArray) ((JSONObject) ((JSONObject) jsonObject.get("result")).get("XXMRZUSD")).get(
-                    "a")).get(0)),
+            jsonObject -> {
+                try {
+                    return ((JSONArray) ((JSONObject) ((JSONObject) jsonObject.get("result")).get("XXMRZUSD")).get(
+                            "a")).get(0);
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            }),
     BTC_RUB_BINANCE("https://api1.binance.com/api/v3/avgPrice?symbol=BTCRUB",
-            jsonObject -> jsonObject.get("price")),
+            jsonObject -> {
+                try {
+                    return jsonObject.get("price");
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            }),
     LTC_RUB_BINANCE("https://api1.binance.com/api/v3/avgPrice?symbol=LTCRUB",
-            jsonObject -> jsonObject.get("price")),
+            jsonObject -> {
+                try {
+                    return jsonObject.get("price");
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            }),
     BTC_RUB_COINGECKO("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=rub",
-            jsonObject -> ((JSONObject) jsonObject.get("bitcoin")).get("rub")),
+            jsonObject -> {
+                try {
+                    return ((JSONObject) jsonObject.get("bitcoin")).get("rub");
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            }),
     LTC_RUB_COINGECKO("https://api.coingecko.com/api/v3/simple/price?ids=litecoin&vs_currencies=rub",
-            jsonObject -> ((JSONObject) jsonObject.get("litecoin")).get("rub")),
+            jsonObject -> {
+                try {
+                    return ((JSONObject) jsonObject.get("litecoin")).get("rub");
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            }),
     USD_RUB_EXCHANGERATE("https://v6.exchangerate-api.com/v6/8ae628548cbe656cdc6f0a9e/latest/USD",
-            jsonObject -> ((JSONObject) jsonObject.get("conversion_rates")).get("RUB"));
+            jsonObject -> {
+                try {
+                    return ((JSONObject) jsonObject.get("conversion_rates")).get("RUB");
+                } catch (JSONException e) {
+                    throw new BaseException("Ошибка при парсинге json курса.");
+                }
+            });
 
     final String url;
 
@@ -61,22 +128,22 @@ public enum CryptoApi {
 
     public static List<CryptoApi> getCryptoApis(CryptoCurrency cryptoCurrency) {
         switch (cryptoCurrency) {
-        case BITCOIN:
-            return BTC_USD;
-        case LITECOIN:
-            return LTS_USD;
-        case MONERO:
-            return XMR_USD;
+            case BITCOIN:
+                return BTC_USD;
+            case LITECOIN:
+                return LTS_USD;
+            case MONERO:
+                return XMR_USD;
         }
         throw new BaseException("API USD для криптовалюты " + cryptoCurrency.name() + " не предусмотрены.");
     }
 
     public static List<CryptoApi> getFiatCryptoApis(CryptoCurrency cryptoCurrency) {
         switch (cryptoCurrency) {
-        case BITCOIN:
-            return BTC_RUB;
-        case LITECOIN:
-            return LTC_RUB;
+            case BITCOIN:
+                return BTC_RUB;
+            case LITECOIN:
+                return LTC_RUB;
         }
         throw new BaseException("API RUB для криптовалюты " + cryptoCurrency.name() + " не предусмотрены.");
     }
