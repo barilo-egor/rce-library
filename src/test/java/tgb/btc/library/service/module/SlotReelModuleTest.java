38792281925dfd18c68fdc496b65a6394e619c74
barilo <barilo>
2024-07-18 22:52:18 +0300

    fixes

--- src/test/java/tgb/btc/library/service/module/SlotReelModuleTest.java
+++ src/test/java/tgb/btc/library/service/module/SlotReelModuleTest.java
@@ -3,9 +3,6 @@ package tgb.btc.library.service.module;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,7 +12,6 @@ import tgb.btc.library.service.properties.GamesPropertiesReader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,28 +26,26 @@ class SlotReelModuleTest {
     @Test
     @DisplayName("Должен закешировать инстанс.")
     void getCurrentCache() {
-        doReturn("NONE").when(gamesPropertiesReader).getString("slot.reel");
+        doReturn("NONE").when(gamesPropertiesReader).getString("slot.reel", SlotReelType.NONE.name());
         slotReelTypeModule.getCurrent();
         slotReelTypeModule.getCurrent();
-        verify(gamesPropertiesReader, times(1)).getString("slot.reel");
-        verify(gamesPropertiesReader, times(1)).getString(anyString());
+        verify(gamesPropertiesReader, times(1)).getString("slot.reel", SlotReelType.NONE.name());
     }
 
     @Test
     @DisplayName("Получение NONE с инстансом и без.")
     void getCurrentNone() {
-        doReturn("NONE").when(gamesPropertiesReader).getString("slot.reel");
+        doReturn("NONE").when(gamesPropertiesReader).getString("slot.reel", SlotReelType.NONE.name());
         SlotReelType expected = SlotReelType.NONE;
         assertEquals(expected, slotReelTypeModule.getCurrent());
         assertEquals(expected, slotReelTypeModule.getCurrent());
-        verify(gamesPropertiesReader, times(1)).getString("slot.reel");
-        verify(gamesPropertiesReader, times(1)).getString(anyString());
+        verify(gamesPropertiesReader, times(1)).getString("slot.reel", SlotReelType.NONE.name());
     }
 
     @Test
     @DisplayName("Получение STANDARD.")
     void getCurrentStandard() {
-        doReturn("STANDARD").when(gamesPropertiesReader).getString("slot.reel");
+        doReturn("STANDARD").when(gamesPropertiesReader).getString("slot.reel", SlotReelType.NONE.name());
         SlotReelType expected = SlotReelType.STANDARD;
         assertEquals(expected, slotReelTypeModule.getCurrent());
     }
@@ -59,7 +53,7 @@ class SlotReelModuleTest {
     @Test
     @DisplayName("Получение STANDARD_ADMIN.")
     void getCurrentStandardAdmin() {
-        doReturn("STANDARD_ADMIN").when(gamesPropertiesReader).getString("slot.reel");
+        doReturn("STANDARD_ADMIN").when(gamesPropertiesReader).getString("slot.reel", SlotReelType.NONE.name());
         SlotReelType expected = SlotReelType.STANDARD_ADMIN;
         assertEquals(expected, slotReelTypeModule.getCurrent());
     }
@@ -67,17 +61,8 @@ class SlotReelModuleTest {
     @Test
     @DisplayName("Должен бросить исключение при невалидном значении.")
     void getCurrentNotValid() {
-        doReturn("qwe").when(gamesPropertiesReader).getString("slot.reel");
+        doReturn("qwe").when(gamesPropertiesReader).getString("slot.reel", SlotReelType.NONE.name());
         BaseException baseException = assertThrows(BaseException.class, () -> slotReelTypeModule.getCurrent());
         assertEquals(baseException.getCause().getClass(), IllegalArgumentException.class);
-    }
-
-    @ParameterizedTest
-    @DisplayName("Должен бросить исключение при не найденном значении.")
-    @NullSource
-    @EmptySource
-    void getCurrentNoValue(String value) {
-        doReturn(value).when(gamesPropertiesReader).getString("slot.reel");
-        assertThrows(BaseException.class, () -> slotReelTypeModule.getCurrent());
     }
 }
