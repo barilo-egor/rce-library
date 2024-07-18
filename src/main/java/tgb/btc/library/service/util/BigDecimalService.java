577923e6212e03d560eeb3ed3b1cbe702ab079b7
barilo <e.barilo@isida.by>
2024-07-18 17:34:27 +0300

    BigDecimalServiceTest

--- src/main/java/tgb/btc/library/service/util/BigDecimalService.java
+++ src/main/java/tgb/btc/library/service/util/BigDecimalService.java
@@ -1,10 +1,13 @@
 package tgb.btc.library.service.util;
 
 import org.springframework.stereotype.Service;
+import tgb.btc.library.constants.enums.bot.CryptoCurrency;
 import tgb.btc.library.interfaces.util.IBigDecimalService;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Objects;
 
 @Service
@@ -12,9 +15,12 @@ public class BigDecimalService implements IBigDecimalService {
 
     private static final BigDecimal HUNDRED = BigDecimal.valueOf(100);
 
-    private static final int SCALE = 20;
+    private static final int SCALE = Arrays.stream(CryptoCurrency.values())
+            .max(Comparator.comparingInt(CryptoCurrency::getScale))
+            .map(CryptoCurrency::getScale)
+            .orElse(8);
 
-    private int getScale() {
+    int getScale() {
         return SCALE;
     }
 
@@ -25,28 +31,35 @@ public class BigDecimalService implements IBigDecimalService {
 
     @Override
     public BigDecimal divideHalfUp(BigDecimal a, BigDecimal b) {
-        return a.setScale(getScale(), RoundingMode.HALF_UP).divide(b, RoundingMode.HALF_UP);
+        if (b.compareTo(BigDecimal.ZERO) == 0) throw new ArithmeticException("Деление на ноль.");
+        if (a.compareTo(BigDecimal.ZERO) == 0) return BigDecimal.ZERO;
+        return setScaleHalfUp(a.divide(b, RoundingMode.HALF_UP));
     }
 
     @Override
     public BigDecimal multiplyHalfUp(BigDecimal a, BigDecimal b) {
-        return a.setScale(getScale(), RoundingMode.HALF_UP).multiply(b);
+        if (BigDecimal.ZERO.equals(a) || BigDecimal.ZERO.equals(b)) return BigDecimal.ZERO;
+        return setScaleHalfUp(a.multiply(b));
     }
 
     @Override
     public BigDecimal addHalfUp(BigDecimal a, BigDecimal b) {
-        return a.setScale(getScale(), RoundingMode.HALF_UP).add(b);
+        return setScaleHalfUp(a.add(b));
     }
 
     @Override
     public BigDecimal subtractHalfUp(BigDecimal a, BigDecimal b) {
-        return a.setScale(getScale(), RoundingMode.HALF_UP).subtract(b);
+        return setScaleHalfUp(a.subtract(b));
+    }
+
+    private BigDecimal setScaleHalfUp(BigDecimal bigDecimal) {
+        return bigDecimal.setScale(getScale(), RoundingMode.HALF_UP);
     }
 
     @Override
     public BigDecimal roundNullSafe(BigDecimal num, int scale) {
-        if (Objects.isNull(num)) return BigDecimal.valueOf(0);
-        return num.setScale(scale, RoundingMode.HALF_UP).stripTrailingZeros();
+        if (Objects.isNull(num)) return BigDecimal.ZERO;
+        return round(num, scale);
     }
 
     @Override
@@ -55,13 +68,13 @@ public class BigDecimalService implements IBigDecimalService {
     }
 
     @Override
-    public String roundToPlainString(BigDecimal num, int scale) {
-        return toPlainString(round(num, scale));
+    public String roundToPlainString(BigDecimal num) {
+        return roundToPlainString(num, 0);
     }
 
     @Override
-    public String roundToPlainString(BigDecimal num) {
-        return roundToPlainString(num, 0);
+    public String roundToPlainString(BigDecimal num, int scale) {
+        return toPlainString(round(num, scale));
     }
 
     @Override
@@ -71,7 +84,7 @@ public class BigDecimalService implements IBigDecimalService {
 
     @Override
     public BigDecimal round(Double num, int scale) {
-        return BigDecimal.valueOf(num).setScale(scale, RoundingMode.HALF_UP).stripTrailingZeros();
+        return round(BigDecimal.valueOf(num), scale);
     }
 
     @Override
