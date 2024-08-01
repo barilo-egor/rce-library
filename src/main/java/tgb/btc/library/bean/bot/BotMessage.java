47e105841b7df0215cfade38cab391f74106e850
barilo <e.barilo@isida.by>
2024-08-01 14:20:29 +0300

    BotMessageRepositoryTest

--- src/main/java/tgb/btc/library/bean/bot/BotMessage.java
+++ src/main/java/tgb/btc/library/bean/bot/BotMessage.java
@@ -1,16 +1,20 @@
 package tgb.btc.library.bean.bot;
 
-import lombok.Builder;
+import lombok.*;
 import tgb.btc.library.bean.BasePersist;
 import tgb.btc.library.constants.enums.bot.BotMessageType;
 import tgb.btc.library.constants.enums.bot.MessageType;
 
 import javax.persistence.*;
-import java.util.Objects;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "BOT_MESSAGE")
 @Builder
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class BotMessage extends BasePersist {
     @Column(name = "TYPE")
     @Enumerated(value = EnumType.STRING)
@@ -24,71 +28,6 @@ public class BotMessage extends BasePersist {
     @Column(name = "MESSAGE_TYPE", nullable = false)
     @Enumerated(value = EnumType.STRING)
     private MessageType messageType = MessageType.TEXT;
-
-    public BotMessage() {
-    }
-
-    public BotMessage(BotMessageType type, String text, String image, String animation, MessageType messageType) {
-        this.type = type;
-        this.text = text;
-        this.image = image;
-        this.animation = animation;
-        this.messageType = messageType;
-    }
-
-    public MessageType getMessageType() {
-        return messageType;
-    }
-
-    public void setMessageType(MessageType messageType) {
-        this.messageType = messageType;
-    }
-
-    public String getAnimation() {
-        return animation;
-    }
-
-    public void setAnimation(String animation) {
-        this.animation = animation;
-    }
-
-    public BotMessageType getType() {
-        return type;
-    }
-
-    public void setType(BotMessageType type) {
-        this.type = type;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        BotMessage that = (BotMessage) o;
-        return type == that.type && Objects.equals(text, that.text) && Objects.equals(image, that.image) && Objects.equals(animation, that.animation) && messageType == that.messageType;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), type, text, image, animation, messageType);
-    }
 
     @Override
     public String toString() {
