e4004c132cff467000fb1ecb9b6f7ab3dc173019
barilo <e.barilo@isida.by>
2024-08-05 17:41:57 +0300

    observer role

--- src/main/java/tgb/btc/library/constants/enums/bot/UserRole.java
+++ src/main/java/tgb/btc/library/constants/enums/bot/UserRole.java
@@ -4,10 +4,13 @@ import java.util.Set;
 
 public enum UserRole {
     USER("Пользователь"),
+    OBSERVER("Наблюдатель"),
     OPERATOR("Оператор"),
     ADMIN("Администратор");
 
-    public static final Set<UserRole> USER_ACCESS = Set.of(UserRole.USER, UserRole.OPERATOR, UserRole.ADMIN);
+    public static final Set<UserRole> USER_ACCESS = Set.of(UserRole.USER, UserRole.OBSERVER, UserRole.OPERATOR, UserRole.ADMIN);
+
+    public static final Set<UserRole> OBSERVER_ACCESS = Set.of(UserRole.OBSERVER, UserRole.OPERATOR, UserRole.ADMIN);
 
     public static final Set<UserRole> OPERATOR_ACCESS = Set.of(UserRole.OPERATOR, UserRole.ADMIN);
 
