5965b18c25ddbdbd819b2c9f644ba1631240dd22
barilo <e.barilo@isida.by>
2024-08-09 19:34:48 +0300

    isOn

--- src/main/java/tgb/btc/library/bean/web/api/ApiRequisite.java
+++ src/main/java/tgb/btc/library/bean/web/api/ApiRequisite.java
@@ -1,8 +1,10 @@
 package tgb.btc.library.bean.web.api;
 
+import com.sun.istack.NotNull;
 import lombok.*;
 import tgb.btc.library.bean.BasePersist;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -21,4 +23,8 @@ public class ApiRequisite extends BasePersist {
     private ApiPaymentType apiPaymentType;
 
     private String requisite;
+
+    @NotNull
+    @Column(nullable = false)
+    private Boolean isOn;
 }
