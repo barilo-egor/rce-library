35721d125b8fdc85f453028a09159a22919886fc
barilo <barilo>
2024-08-12 21:23:54 +0300

    currencies

--- src/main/java/tgb/btc/library/bean/web/api/ApiPaymentType.java
+++ src/main/java/tgb/btc/library/bean/web/api/ApiPaymentType.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.*;
 import tgb.btc.library.bean.BasePersist;
+import tgb.btc.library.constants.enums.bot.CryptoCurrency;
+import tgb.btc.library.constants.enums.bot.DealType;
+import tgb.btc.library.constants.enums.bot.FiatCurrency;
 import tgb.btc.library.interfaces.JsonConvertable;
 
 import javax.persistence.Entity;
@@ -25,12 +28,20 @@ public class ApiPaymentType extends BasePersist implements JsonConvertable {
 
     private String comment;
 
+    private DealType dealType;
+
+    private FiatCurrency fiatCurrency;
+
+    private CryptoCurrency cryptoCurrency;
+
     @Override
     public ObjectNode map() {
         return new ObjectMapper()
                 .createObjectNode()
                 .put("id", id)
                 .put("name", name)
-                .put("comment", comment);
+                .put("comment", comment)
+                .put("dealType", dealType.getNominativeFirstLetterToUpper())
+                .put("cryptoCurrency", cryptoCurrency.name());
     }
 }
