cfb2f7d0a6bd14d19dd47ceadf8bd893fdce62ff
barilo <barilo>
2024-08-12 21:47:53 +0300

    findAll(DealType dealType)

--- src/main/java/tgb/btc/library/service/bean/web/ApiPaymentTypeService.java
+++ src/main/java/tgb/btc/library/service/bean/web/ApiPaymentTypeService.java
@@ -1,12 +1,16 @@
 package tgb.btc.library.service.bean.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 import tgb.btc.library.bean.web.api.ApiPaymentType;
+import tgb.btc.library.constants.enums.bot.DealType;
 import tgb.btc.library.interfaces.service.bean.web.IApiPaymentTypeService;
 import tgb.btc.library.repository.BaseRepository;
 import tgb.btc.library.repository.web.ApiPaymentTypeRepository;
 import tgb.btc.library.service.bean.BasePersistService;
+
+import java.util.List;
 
 @Service
 public class ApiPaymentTypeService extends BasePersistService<ApiPaymentType> implements IApiPaymentTypeService {
@@ -21,5 +25,9 @@ public class ApiPaymentTypeService extends BasePersistService<ApiPaymentType> im
     @Override
     protected BaseRepository<ApiPaymentType> getBaseRepository() {
         return apiPaymentTypeRepository;
+    }
+
+    public List<ApiPaymentType> findAll(DealType dealType) {
+        return apiPaymentTypeRepository.findAll(Example.of(ApiPaymentType.builder().dealType(dealType).build()));
     }
 }
