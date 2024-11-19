8140e9bc8eb9f324c72e08986d044ba3c0ec4a68
barilo <e.barilo@isida.by>
2024-11-19 13:52:15 +0300

    remove old DICE_MESSAGE reader

--- src/main/java/tgb/btc/library/service/process/DiceService.java
+++ src/main/java/tgb/btc/library/service/process/DiceService.java
@@ -1,36 +1,44 @@
 package tgb.btc.library.service.process;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-import tgb.btc.library.constants.enums.properties.PropertiesPath;
+import tgb.btc.library.service.properties.DiceMessagePropertiesReader;
 
 @Service
 @Slf4j
 public class DiceService {
 
+    private DiceMessagePropertiesReader diceMessagePropertiesReader;
+
+    @Autowired
+    public void setDiceMessagePropertiesReader(DiceMessagePropertiesReader diceMessagePropertiesReader) {
+        this.diceMessagePropertiesReader = diceMessagePropertiesReader;
+    }
+
     @Cacheable("diceSelectWinNumberMessage")
     public String selectBetMessage(){
-        return PropertiesPath.DICE_MESSAGE.getString("start")+ System.lineSeparator()+ System.lineSeparator() +
-                PropertiesPath.DICE_MESSAGE.getString("start.rules")+ System.lineSeparator()+ System.lineSeparator() +
-                "*" + PropertiesPath.DICE_MESSAGE.getString("referral.balance") + "* %s₽"+ System.lineSeparator()+ System.lineSeparator() +
-                PropertiesPath.DICE_MESSAGE.getString("start.triple.sum");
+        return diceMessagePropertiesReader.getString("start")+ System.lineSeparator()+ System.lineSeparator() +
+                diceMessagePropertiesReader.getString("start.rules")+ System.lineSeparator()+ System.lineSeparator() +
+                "*" + diceMessagePropertiesReader.getString("referral.balance") + "* %s₽"+ System.lineSeparator()+ System.lineSeparator() +
+                diceMessagePropertiesReader.getString("start.triple.sum");
     }
 
     @Cacheable("diceWinMessage")
     public String winMessage(){
-        return "*" + PropertiesPath.DICE_MESSAGE.getString("win") + "* %s" + System.lineSeparator() + System.lineSeparator() +
+        return "*" + diceMessagePropertiesReader.getString("win") + "* %s" + System.lineSeparator() + System.lineSeparator() +
                 "*Вы утроили: %s*₽!"+ System.lineSeparator() + System.lineSeparator() +
-                "*" + PropertiesPath.DICE_MESSAGE.getString("referral.balance") + "* %s₽";
+                "*" + diceMessagePropertiesReader.getString("referral.balance") + "* %s₽";
 
     }
 
     @Cacheable("diceLoseMessage")
     public String loseMessage(){
-        return PropertiesPath.DICE_MESSAGE.getString("lose") + " %s" + System.lineSeparator() + System.lineSeparator() +
-                PropertiesPath.DICE_MESSAGE.getString("selected.number") + " %s"
+        return diceMessagePropertiesReader.getString("lose") + " %s" + System.lineSeparator() + System.lineSeparator() +
+                diceMessagePropertiesReader.getString("selected.number") + " %s"
                 + System.lineSeparator() + System.lineSeparator() +
-                "*" + PropertiesPath.DICE_MESSAGE.getString("referral.balance") + "* %s₽";
+                "*" + diceMessagePropertiesReader.getString("referral.balance") + "* %s₽";
     }
 
 }
