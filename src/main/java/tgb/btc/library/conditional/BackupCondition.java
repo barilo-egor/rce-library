6f3c3760d6c51f0859a21649357b34653dce9e1d
barilo <e.barilo@isida.by>
2024-11-19 13:30:48 +0300

    remove old FUNCTIONS_PROPERTIES reader

--- src/main/java/tgb/btc/library/conditional/BackupCondition.java
+++ src/main/java/tgb/btc/library/conditional/BackupCondition.java
@@ -3,12 +3,15 @@ package tgb.btc.library.conditional;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
-import tgb.btc.library.constants.enums.properties.PropertiesPath;
+import tgb.btc.library.service.properties.FunctionsPropertiesReader;
+
+import java.util.Objects;
 
 public class BackupCondition implements Condition {
 
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        return PropertiesPath.FUNCTIONS_PROPERTIES.getBoolean("auto.backup", Boolean.FALSE);
+        return Objects.requireNonNull(context.getBeanFactory()).getBean(FunctionsPropertiesReader.class)
+                .getBoolean("auto.backup", Boolean.FALSE);
     }
 }
