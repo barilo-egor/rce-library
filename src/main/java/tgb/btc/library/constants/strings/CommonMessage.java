0a25f019bed1562675e6d738b3263ccf9e0477ec
barilo <e.barilo@isida.by>
2025-01-23 18:07:49 +0300

    add to pool message + confirm message

--- src/main/java/tgb/btc/library/constants/strings/CommonMessage.java
+++ src/main/java/tgb/btc/library/constants/strings/CommonMessage.java
@@ -6,4 +6,8 @@ public class CommonMessage {
     }
 
     public static final String INPUT_WALLET = "Введите %s-адрес кошелька, куда вы хотите отправить %s%s.%s";
+
+    public static final String DEAL_CONFIRMED = """
+            Валюта отправлена.
+            %s""";
 }
