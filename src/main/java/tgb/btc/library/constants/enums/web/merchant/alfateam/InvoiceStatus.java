4e59396fb2e13b74ee1cd7c9389acf72c116fd81
barilo <barilo>
2025-03-16 12:27:45 +0300

    fix invoice status

--- src/main/java/tgb/btc/library/constants/enums/web/merchant/alfateam/InvoiceStatus.java
+++ src/main/java/tgb/btc/library/constants/enums/web/merchant/alfateam/InvoiceStatus.java
@@ -1,6 +1,5 @@
 package tgb.btc.library.constants.enums.web.merchant.alfateam;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
@@ -14,12 +13,11 @@ import java.io.IOException;
 @Getter
 @Slf4j
 public enum InvoiceStatus {
-    NEW("new", "Сделка только что создана."),
-    TRANSFER_WAITING("transfer_waiting", "Ожидается перевод средств."),
-    TRANSFER_CONFIRMED("transfer_confirmed", "Перевод средств подтвержден пользователем."),
-    CANCELED("canceled", "Сделка была отменена."),
-    COMPLETED("completed", "Сделка успешно завершена."),
-    DISPUTE("dispute", "Сделка находится в стадии спора.");
+    NEW("new", "Инвойс только что создан."),
+    PAID("paid", "Инвойс был оплачен."),
+    CANCELED("canceled", "Инвойс был отменен."),
+    EXPIRED("expired", "Инвойс истек."),
+    DISPUTE("dispute", "Инвойс находится в стадии спора.");
 
     private final String value;
 
@@ -36,8 +34,7 @@ public enum InvoiceStatus {
 
     public static class Deserializer extends JsonDeserializer<InvoiceStatus> {
         @Override
-        public InvoiceStatus deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
-            log.info("Парсинг InvoiceStatus: {}", jsonParser.getValueAsString());
+        public InvoiceStatus deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
             return InvoiceStatus.getByValue(jsonParser.getValueAsString());
         }
     }
